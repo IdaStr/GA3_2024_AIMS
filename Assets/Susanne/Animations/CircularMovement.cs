aa6adabd0f14bc48acf0236b4dd7d2fd7854844b
Suzzi94 <Bergersen.susanne@gmail.com>
2024-04-16 17:29:28 +0200

    Added Animations

---
+++ Assets/Susanne/Animations/CircularMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovement : MonoBehaviour
+{
+    public float radius = 2f;      
+    public float speed = 1f;       
+
+    private Vector3 centerPosition;
+    private Vector3 lastPosition;
+
+    void Start()
+    {
+        
+        if (transform != null)
+        {
+            
+            centerPosition = transform.position;
+            lastPosition = transform.position;
+        }
+        else
+        {
+            Debug.LogError("Transform component is null in CircularMovement script.");
+        }
+    }
+
+    void Update()
+    {
+        
+        float angle = Time.time * speed;    
+        float x = centerPosition.x + radius * Mathf.Cos(angle);  
+        float z = centerPosition.z + radius * Mathf.Sin(angle);  
+
+        Vector3 newPosition = new Vector3(x, centerPosition.y, z);
+
+        
+        if (transform != null)
+        {
+            transform.position = newPosition;
+
+           
+            Vector3 moveDirection = newPosition - lastPosition;
+            moveDirection.y = 0f;   
+
+           
+            if (moveDirection != Vector3.zero)
+            {
+                Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+                transform.rotation = newRotation;
+            }
+
+            
+            lastPosition = newPosition;
+        }
+    }
+}
+
+
