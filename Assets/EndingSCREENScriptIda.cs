bd2eab1776e6383a855912738b82276e8927c0ed
IdaStr <ida.v.s@hotmail.no>
2024-05-01 17:22:23 +0200

    ida ass code attempt
    its in my scene, script is in main asset map,

--- Assets/EndingSCREENScriptIda.cs
+++ Assets/EndingSCREENScriptIda.cs
@@ -1,20 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+
+
 
 public class EndingSCREENScriptIda : MonoBehaviour
 {
+    public Canvas EndCredits;
 
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        { if (other.tag == "Player")
 
-            Debug.Log("Player entered and left triggerzone");
+                Debug.Log("Player entered triggerzone");
+        
 
-      
+        }
 
-    }
+        {
+            if (other.tag == "Player")
+            {
+                Debug.Log("Canvas should activate if youre not an idiot Ida");
+                EndCredits.enabled = false;
+
+            }
+            
+
+
+        }
+
+    }  
 
 
 }
